4efd2010fbc0673df0218432681d84e5ca01dc5f
Alex Tang <alex.m.tang@gmail.com>
2018-08-29 09:00:13 -0700

    Refactor Login.js and fix zIndex

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Font } from 'expo';
+import Login from './Login';
 
-export default class App extends React.Component {
+class App extends React.Component {
   state = {
     fontLoaded: false,
-    username: '',
-    password: ''
   };
 
   async componentDidMount() {
@@ -19,112 +18,9 @@ export default class App extends React.Component {
     this.setState({ fontLoaded: true });
   }
 
-  onPress() {
-    console.log(this.state);
-  }
-
   render() {
-    return (
-      this.state.fontLoaded && (
-        <View style={styles.container}>
-          <View style={styles.top}>
-            <Image source={require('./assets/cannabis.png')} style={styles.logo} />
-            <Text style={styles.title}>cannasseur</Text>
-          </View>
-          <View style={styles.bottom}>
-            <View style={styles.inputs}>
-              <TextInput
-                autoCapitalize={'none'}
-                placeholder={'username'}
-                onChangeText={username => this.setState({ username })}
-                style={styles.input}
-              />
-              <TextInput
-                autoCapitalize={'none'}
-                placeholder={'password'}
-                onChangeText={password => this.setState({ password })}
-                style={styles.input}
-                secureTextEntry
-              />
-            </View>
-            <TouchableOpacity style={styles.login} onPress={() => this.onPress()}>
-              <Text style={styles.loginText}>LOG IN</Text>
-            </TouchableOpacity>
-          </View>
-          {/* <View style={styles.border} /> */}
-        </View>
-      )
-    );
+    return this.state.fontLoaded && <Login />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    top: '30%'
-  },
-  border: {
-    borderColor: '#000',
-    borderWidth: 2,
-    height: '90%',
-    width: '90%',
-    position: 'absolute',
-    zIndex: 100,
-    left: '5%',
-    top: '5%'
-  },
-  top: {
-    height: '50%',
-    alignItems: 'center',
-    backgroundColor: '#F4F3EF'
-  },
-  title: {
-    top: '30%',
-    fontSize: 40,
-    fontFamily: 'PlayfairDisplay-Italic'
-  },
-  bottom: {
-    height: '50%',
-    // backgroundColor: '#F4F3EF'
-  },
-  inputs: {
-    top: '10%',
-    left: '15%',
-    zIndex: 200,
-    position: 'absolute',
-    width: '70%'
-  },
-  input: {
-    fontFamily: 'PlayfairDisplay-Regular',
-    marginBottom: 8,
-    fontSize: 20,
-    borderColor: '#000',
-    borderBottomWidth: 1,
-    padding: 8
-  },
-  login: {
-    width: '80%',
-    height: 48,
-    backgroundColor: '#000',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    left: '10%',
-    borderRadius: 8,
-    top: '40%',
-    position: 'absolute',
-    zIndex: 200
-  },
-  loginText: {
-    color: '#fff',
-    fontFamily: 'WorkSans',
-    fontSize: 16,
-    position: 'absolute',
-    zIndex: 200
-  }
-});
+export default App;
---
+++ Login.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
+  onPress() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.top}>
+          <Image source={require('./assets/cannabis.png')} style={styles.logo} />
+          <Text style={styles.title}>cannasseur</Text>
+        </View>
+        <View style={styles.bottom}>
+          <View style={styles.inputs}>
+            <TextInput
+              autoCapitalize={'none'}
+              placeholder={'username'}
+              onChangeText={username => this.setState({ username })}
+              style={styles.input}
+            />
+            <TextInput
+              autoCapitalize={'none'}
+              placeholder={'password'}
+              onChangeText={password => this.setState({ password })}
+              style={styles.input}
+              secureTextEntry
+            />
+          </View>
+          <TouchableOpacity style={styles.login} onPress={() => this.onPress()}>
+            <Text style={styles.loginText}>LOG IN</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.border} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 200,
+    height: 200,
+    top: '30%'
+  },
+  border: {
+    borderColor: '#000',
+    borderWidth: 2,
+    height: '90%',
+    width: '90%',
+    position: 'absolute',
+    zIndex: 100,
+    left: '5%',
+    top: '5%'
+  },
+  top: {
+    height: '50%',
+    alignItems: 'center',
+    backgroundColor: '#F4F3EF'
+  },
+  title: {
+    top: '30%',
+    fontSize: 40,
+    fontFamily: 'PlayfairDisplay-Italic'
+  },
+  bottom: {
+    height: '50%',
+    zIndex: 200,
+  },
+  inputs: {
+    top: '10%',
+    left: '15%',
+    position: 'absolute',
+    width: '70%'
+  },
+  input: {
+    fontFamily: 'PlayfairDisplay-Regular',
+    marginBottom: 8,
+    fontSize: 20,
+    borderColor: '#000',
+    borderBottomWidth: 1,
+    padding: 8
+  },
+  login: {
+    width: '80%',
+    height: 48,
+    backgroundColor: '#000',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: '10%',
+    borderRadius: 8,
+    top: '40%',
+    position: 'absolute',
+  },
+  loginText: {
+    color: '#fff',
+    fontFamily: 'WorkSans',
+    fontSize: 16,
+    position: 'absolute',
+  }
+});
+
+export default Login;
