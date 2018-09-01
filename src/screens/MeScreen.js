90f13bf7eb47c65beedee4456e6767554fef12c5
Alex Tang <alex.m.tang@gmail.com>
2018-08-31 17:18:36 -0700

    Update MeScreen with log out button

--- src/screens/MeScreen.js
+++ src/screens/MeScreen.js
@@ -9,11 +9,23 @@ import {
   Image,
   FlatList
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import * as firebase from 'firebase';
 
 class MeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'PROFILE',
+      headerLeft: (
+        <TouchableOpacity style={{ left: 16 }} onPress={() => navigation.goBack()}>
+          <Ionicons name={'ios-arrow-dropleft'} size={32} />
+        </TouchableOpacity>
+      )
+    };
+  };
+
   async logout() {
-    console.log('logging out')
+    console.log('logging out');
     await AsyncStorage.clear();
     await firebase.auth().signOut();
     this.props.navigation.navigate('AuthLoading');
@@ -21,11 +33,29 @@ class MeScreen extends React.Component {
 
   render() {
     return (
-      <TouchableOpacity onPress={() => this.logout()}>
-        <Text>LOG OUT</Text>
+      <TouchableOpacity style={styles.logoutButton} onPress={() => this.logout()}>
+        <Text style={styles.logoutText}>LOG OUT</Text>
       </TouchableOpacity>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    top: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
+    width: '80%',
+    backgroundColor: '#000',
+    borderRadius: 8,
+    left: '10%'
+  },
+  logoutText: {
+    color: '#FFF',
+    fontFamily: 'WorkSans',
+    fontSize: 16
+  }
+});
 
 export default MeScreen;
