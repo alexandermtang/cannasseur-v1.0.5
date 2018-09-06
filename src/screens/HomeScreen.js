5d82753777f90441c22d6434ef41b7c032326d16
Alex Tang <alex.m.tang@gmail.com>
2018-09-06 15:59:26 -0700

    Implement Filter function

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -32,7 +32,9 @@ class HomeScreen extends React.Component {
     allLogs: [],
     filteredLogs: [],
     isLoading: true,
-    isEmpty: false
+    isEmpty: false,
+    showModal: false,
+    filterText: 'MOST RECENT'
   };
 
   async getLogs() {
@@ -85,20 +87,54 @@ class HomeScreen extends React.Component {
     }
   }
 
+  showModal() {
+    this.setState({ showModal: true });
+  }
+
+  hideModal() {
+    this.setState({ showModal: false });
+  }
+
+  sortBy(type = 'mostRecent') {
+    const tempLogs = [...this.state.allLogs];
+    let filterText = '';
+
+    if (type === 'mostRecent') {
+      tempLogs.sort((a, b) => new Date(b.date) - new Date(a.date));
+      filterText = 'MOST RECENT';
+    } else if (type === 'topRated') {
+      tempLogs.sort((a, b) => b.finalRating - a.finalRating);
+      filterText = 'TOP RATED';
+    } else {
+      tempLogs.sort((a, b) => b[type] - a[type]);
+      filterText = `MOOD: ${type.toUpperCase()}`;
+    }
+    console.log(tempLogs);
+
+    this.setState({ filteredLogs: tempLogs, showModal: false, filterText });
+  }
+
   render() {
     const strainsSet = new Set();
     this.state.allLogs.forEach(log => strainsSet.add(log.strain));
     const numStrains = strainsSet.size;
+    console.log('logs', this.state.filteredLogs);
 
     return (
       <View style={styles.container}>
-        <View style={styles.searchInputContainer}>
-          <Ionicons style={styles.searchIcon} name={'ios-search'} size={32} />
-          <TextInput
-            style={styles.searchInput}
-            placeholder={'Search strain or tag'}
-            onChangeText={searchText => this.search(searchText)}
-          />
+        <View style={styles.headerContainer}>
+          <View>
+            <Ionicons style={styles.searchIcon} name={'ios-search'} size={32} />
+            <TextInput
+              style={styles.searchInput}
+              placeholder={'Search strain or tag'}
+              onChangeText={searchText => this.search(searchText)}
+            />
+          </View>
+          <TouchableOpacity style={styles.filterContainer} onPress={() => this.showModal()}>
+            <Ionicons style={styles.filterIcon} name={'ios-funnel'} size={32} />
+            <Text style={styles.filterText}>{this.state.filterText}</Text>
+          </TouchableOpacity>
         </View>
         {this.state.isLoading ? (
           <ActivityIndicator size="large" color="#9b9b9b" />
@@ -131,10 +167,46 @@ class HomeScreen extends React.Component {
             </Text>
           </View>
         </View>
+        {this.state.showModal && (
+          <View style={styles.modal}>
+            <TouchableOpacity style={styles.top} onPress={() => this.hideModal()} />
+            <View style={styles.bottom}>
+              <View style={styles.modalHeaderContainer}>
+                <Ionicons
+                  style={styles.closeIcon}
+                  name={'ios-close'}
+                  size={32}
+                  onPress={() => this.hideModal()}
+                />
+                <Text style={styles.filterOptionsHeaderText}>Filter Options</Text>
+              </View>
+              <ScrollView style={{ paddingBottom: 56 }}>
+                <FilterButton onPress={() => this.sortBy('mostRecent')} text={'MOST RECENT'} />
+                <FilterButton onPress={() => this.sortBy('topRated')} text={'TOP RATED'} />
+                <FilterButton onPress={() => this.sortBy('happy')} text={'MOOD: HAPPY'} />
+                <FilterButton onPress={() => this.sortBy('creative')} text={'MOOD: CREATIVE'} />
+                <FilterButton onPress={() => this.sortBy('active')} text={'MOOD: ACTIVE'} />
+                <FilterButton onPress={() => this.sortBy('relaxed')} text={'MOOD: RELAXED'} />
+                <FilterButton onPress={() => this.sortBy('hungry')} text={'MOOD: HUNGRY'} />
+                <FilterButton onPress={() => this.sortBy('sleepy')} text={'MOOD: SLEEPY'} />
+              </ScrollView>
+              <View />
+            </View>
+          </View>
+        )}
       </View>
     );
   }
 }
+
+const FilterButton = ({ onPress, text }) => (
+  <TouchableOpacity
+    style={{ alignItems: 'center', justifyContent: 'center', height: 56 }}
+    onPress={() => onPress()}
+  >
+    <Text style={{ fontSize: 16, fontFamily: 'WorkSans', lineHeight: 56 }}>{text}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -143,11 +215,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     backgroundColor: '#FFF'
   },
-  searchInputContainer: {
-    // display: 'flex'
-    // alignItems: 'center',
-    // justifyContent: 'flex-start'
-  },
+  headerContainer: {},
   searchIcon: {
     position: 'absolute',
     zIndex: 100,
@@ -163,6 +231,23 @@ const styles = StyleSheet.create({
     fontSize: 24,
     borderBottomWidth: 1,
     borderColor: '#d8d8d8'
+  },
+  filterContainer: {
+    height: 56,
+    paddingRight: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#d8d8d8'
+  },
+  filterIcon: {
+    padding: 8
+  },
+  filterText: {
+    fontSize: 16,
+    fontFamily: 'WorkSans'
   },
   logsContainer: {
     height: '100%',
@@ -189,6 +274,47 @@ const styles = StyleSheet.create({
   strainsRecorded: {
     fontFamily: 'WorkSans',
     fontSize: 16
+  },
+
+  modal: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    zIndex: 500
+    // bottom: 90
+  },
+  top: {
+    height: '40%',
+    width: '100%',
+    backgroundColor: '#000',
+    opacity: 0.5
+  },
+  bottom: {
+    height: '60%',
+    backgroundColor: '#fff'
+  },
+  modalHeaderContainer: {
+    height: 56,
+    borderBottomWidth: 1,
+    borderColor: '#000',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  closeIcon: {
+    position: 'absolute',
+    top: 12,
+    left: 16
+  },
+  filterOptionsHeaderText: {
+    fontSize: 24,
+    fontFamily: 'PlayfairDisplay-Regular'
+  },
+  filterOptionsText: {
+    fontSize: 16,
+    fontFamily: 'WorkSans',
+    padding: 16
   }
 });
 
