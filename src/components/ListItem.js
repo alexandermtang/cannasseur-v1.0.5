7f91e23b75491a43767ce43d8ef442d989c6695a
Alex Tang <alex.m.tang@gmail.com>
2018-09-28 09:06:49 -0700

    Upgrade packages and add stars to ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import moment from 'moment';
 import Swipeout from 'react-native-swipeout';
+import StarRating from 'react-native-star-rating';
 
 const ListItem = ({ item, onPress, onPressDelete }) => {
   const date = moment(item.date).format('MM/DD');
+  const strain = item.strain.length > 22 ? `${item.strain.slice(0, 22)}...` : item.strain;
 
   const swipeoutBtns = [
     {
@@ -17,7 +19,13 @@ const ListItem = ({ item, onPress, onPressDelete }) => {
   return (
     <Swipeout autoClose right={swipeoutBtns}>
       <TouchableOpacity style={styles.container} onPress={onPress}>
-        <Text style={styles.text}>{`${date}\t${item.strain}`}</Text>
+        <Text style={styles.text}>{`${date}\t${strain}`}</Text>
+        <StarRating
+          disabled={true}
+          rating={item.finalRating}
+          starStyle={{ fontSize: 16 }}
+          containerStyle={{ paddingRight: 16, width: '30%' }}
+        />
       </TouchableOpacity>
     </Swipeout>
   );
@@ -27,7 +35,9 @@ const styles = StyleSheet.create({
   container: {
     height: 56,
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingLeft: 16,
     backgroundColor: '#FFF',
     borderColor: '#d8d8d8',
